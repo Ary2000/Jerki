cb8c47eae4aa7cb978f12c55b2059fd5985f816b
Ary-El Durán <aryelduranballestero@gmail.com>
2019-03-30 20:51:41 -0600

    Se intento hacer una ventana

--- Jerky/Jerky/main.cpp
+++ Jerky/Jerky/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "Simple_window.h"
+#include "Graph.h"
 using namespace std;
 
+typedef struct Nodo* enlace;
+struct Nodo {
+	int v;
+	enlace sig;
+	Nodo(int v) : v{ v }, sig{ nullptr } {}
+	Nodo(int v, enlace sig) : v{ v }, sig{ sig } {}
+};
+
 int main() {
-	cout << "Funciona" << endl;
+	Point tl(100, 100);
+	//Simple_window win(Point(200, 200), 600, 400, "Canvas");
+	//win.wait_for_button();
 }
